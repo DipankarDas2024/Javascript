44c3c90be537d79d99028d29c3b79df1fa9d1983
DipankarDas2024 <163502899+DipankarDas2024@users.noreply.github.com>
2024-03-15 09:09:35 +0000

    Datatypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12233
+let accountEmail = "das@gmail.com"
+var accountPass = "12356"
+accountCity = "Medinipur"
+let accountStatus;
+
+// accountId = 4
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPass, accountCity, accountStatus])
