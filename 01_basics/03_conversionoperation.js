d215d024459be2beda5d529018960de45f8c6de7
DipankarDas2024 <163502899+DipankarDas2024@users.noreply.github.com>
2024-03-15 14:13:08 +0000

    save

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,48 @@
+ let score = 33
+
+// console.log(score)
+
+// let p= Number(score)
+
+// console.log(typeof score)
+
+// console.log(typeof(p))
+
+// console.log(p)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+let str = String(score)
+// console.log(str)
+// console.log(typeof str)
+
+
+let str1 = "Debnath"
+let str2 = " Expart"
+let str3 = str1 + str2
+// console.log(str3)
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+console.table([num1, num2, num3])
+
+let gameCounter = 8
+
+++gameCounter
+
+console.log(gameCounter)
+
